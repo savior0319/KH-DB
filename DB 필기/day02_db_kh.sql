a0959838a0bded8307565f0a417a31db835e3a78
AHJ <savior0319@naver.com>
2018-03-22 16:59:54 +0900

    -

--- DB 필기/day02_db_kh.sql
+++ DB 필기/day02_db_kh.sql
@@ -100,7 +100,7 @@ FROM EMPLOYEE
 WHERE EMP_NAME NOT LIKE '이%';
 
 
--- 실습 문제 3 --
+-- 실습 문제 3
 
 -- 문제 1
 SELECT *
@@ -148,3 +148,57 @@ ORDER BY EMP_ID DESC;
 SELECT *
 FROM EMPLOYEE
 ORDER BY  1 DESC;
+
+-- 실습 문제 4
+-- 문제1.
+-- 입사일이 5년 이상, 10년 이하인 직원의 이름,주민번호,급여,입사일을 검색하여라
+
+-- 문제2.
+-- 재직중이 아닌 직원의 이름,부서코드를 검색하여라 (퇴사 여부 : ENT_YN)
+
+-- 문제3.
+-- 근속년수가 10년 이상인 직원들을 검색하여
+-- 출력 결과는 이름,급여,근속년수(소수점X)를 근속년수가 오름차순으로 정렬하여 출력하여라
+-- 단, 급여는 50% 인상된 급여로 출력되도록 하여라.
+
+-- 문제4.
+-- 입사일이 99/01/01 ~ 10/01/01 인 사람 중에서 급여가 2000000 원 이하인 사람의
+-- 이름,주민번호,이메일,폰번호,급여를 검색 하시오
+
+-- 문제5.
+-- 급여가 2000000원 ~ 3000000원 인 여직원 중에서 4월 생일자를 검색하여
+-- 이름,주민번호,급여,부서코드를 주민번호 순으로(내림차순) 출력하여라
+-- 단, 부서코드가 null인 사람은 부서코드가 '없음' 으로 출력 하여라.
+
+-- 문제6.
+-- 남자 사원 중 보너스가 없는 사원의 오늘까지 입사일을 측정하여
+-- 1000일 마다(소수점 제외)
+-- 급여의 10% 보너스를 계산하여 이름,특별 보너스 (계산 금액) 결과를 출력하여라.
+-- 단, 이름 순으로 오름 차순 정렬하여 출력하여라.
+
+SELECT EMP_NAME, EMP_NO, SALARY, HIRE_DATE
+FROM EMPLOYEE
+WHERE (SYSDATE - HIRE_DATE) / 365  BETWEEN 5 AND 10;
+
+SELECT EMP_NAME, DEPT_CODE, ABS((SYSDATE - ENT_DATE) -  (SYSDATE - HIRE_DATE )) AS "근무일 수", HIRE_DATE
+FROM EMPLOYEE
+WHERE ENT_YN NOT LIKE 'N';
+
+SELECT EMP_NAME, SALARY * 1.5 ,FLOOR((SYSDATE - HIRE_DATE) / 365) AS "근속년수"
+FROM EMPLOYEE
+WHERE   (SYSDATE - HIRE_DATE) / 365  >= 10
+ORDER BY 3;
+
+SELECT EMP_NAME, EMP_NO, EMAIL, PHONE, SALARY
+FROM EMPLOYEE
+WHERE SALARY <= 2000000 AND HIRE_DATE BETWEEN '99/01/01' AND '10/01/01';
+
+SELECT EMP_NAME, EMP_NO, SALARY, NVL(DEPT_CODE, '없음')
+FROM EMPLOYEE
+WHERE (SALARY BETWEEN 2000000 AND 3000000) AND (EMP_NO LIKE '___4___2%')
+ORDER BY 2 DESC;
+
+SELECT EMP_NAME AS "이름",  (SALARY * ( FLOOR(FLOOR(SYSDATE - HIRE_DATE) / 1000 ) * 0.1)) || '원' AS "특별 보너스"
+FROM EMPLOYEE
+WHERE (EMP_NO LIKE '_______1%')  AND BONUS IS NULL
+ORDER BY 1;
